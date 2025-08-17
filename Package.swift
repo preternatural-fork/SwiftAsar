799ec4e7a76e309f3b8296b3ddc62d4bb3b4bef7
Vatsal Manot <vatsal.manot@yahoo.com>
2025-08-17 15:39:52 -0700

    Update package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -12,15 +11,12 @@ let package = Package(
         .watchOS(.v7)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftAsar",
             targets: ["SwiftAsar"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftAsar"
         ),
